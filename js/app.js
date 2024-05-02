51b7642a2fd934f026d95a0a16b555a9657d6c31
dnlslcd <danielsalcedoq@gmail.com>
2024-05-02 21:20:54 +0200

    todo acabado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 // Implementar solución aquí
 
-document.querySelector("#btn")
+// Una funcion para generar un color hexadecimal aleatorio:
+function generateHexColor() {
+    let numeroAleatorio = Math.floor(Math.random() * 16777215);
+    // Convierte el número a formato hexadecimal y asegura que tenga 6 dígitos
+    let numeroHexadecimal = numeroAleatorio.toString(16).padStart(6, '0');
+    // Retorna el número hexadecimal generado
+    return '#' + numeroHexadecimal;
+}
+
+
+function changeBG() {
+    console.log("Se está recuperando el objeto del DOM");
+
+// encapsular en una variable el generador de colores hexadecimales
+    const randomHexColor = generateHexColor();
+
+// asignar un color hex random al bkgd
+    document.querySelector("body").style.backgroundColor = randomHexColor;
+
+// asignar el mismo color hex aleatorio al span del texto en la web
+    document.querySelector("#hexValue").textContent = randomHexColor;
+}
+
+
+const btn = document.querySelector("#btn");
+
+// Una función que se va a encargar de gestionar el evento click
+btn.onclick = changeBG, hexValueFunciona;
